ee089432604b28c00eeda0f55ce32986c764eb99
Johannes Neumeier <johannes.neumeier@gmail.com>
2013-03-03 23:00:39 +0200

    initial release v 0.1

--- js/css-specificity-tool.js
+++ js/css-specificity-tool.js
@@ -1,28 +1,69 @@
+var Tool = {};
+
 $(function() {
 
-	$(document).on('click', '.calculate', Tool.calculate);
+	// cached selectors for static elements
+	Tool.$sort = $('.sort');
+	Tool.$add  = $('.add');
+	Tool.$list = $('.selectors');
+	Tool.onResetDefaultTo = '<span class="specificity-a specificity-undefined">?</span>,<span class="specificity-b specificity-undefined">?</span>,<span class="specificity-c specificity-undefined">?</span>,<span class="specificity-d specificity-undefined">?</span>';
+
+	$(document).on('click', '.calculate', Tool.calculateFromEvent);
+	$(document).on('keyup', 'input[name="selector"]', Tool.calculateFromEvent);
 	$(document).on('click', '.remove', Tool.removeRow);
 	$(document).on('click', '.clear', Tool.clearRow);
-	$('.add').on('click', Tool.newRow);
+
+	$(document).on('mouseover', '.specificity-string span', Tool.showMatchesTooltip);
+	$(document).on('mouseout', '.specificity-string span, .specificity-string span ul', Tool.hideMatchesTooltip);
+
+	// on refresh calculate any selector in the first input
+	$('input[name="selector"]').each(function () {
+		if ($(this).val() != '') {		
+			Tool.calculate($(this).parent('li'));
+		}
+	});
+
+	Tool.$sort.on('click', Tool.sortRows);
+	Tool.$add.on('click', Tool.newRow);
 
 });
 
 
-var Tool = {};
+Tool.calculateFromEvent = function () {
+
+	Tool.calculate($(this).parent());
+
+}
 
 
-Tool.calculate = function () {
+Tool.calculate = function ($li) {
 
-	var $li     = $(this).parent(),
-		$clear  = $li.find('.clear'),
+	var $clear  = $li.find('.clear'),
 		$remove = $li.find('.remove'),
-		$input  = $li.find('input[name="selector"]'),
-		$span   = $li.find('.specificity-string');
+		$span   = $li.find('.specificity-string'),
+		$input  = $li.find('input[name="selector"]'),		
+		specificity = Specificity.calculate($input.val());
 
-	$span.html(Specificity.calculate($input.val()).string);
-	$clear.removeAttr('disabled');
-	if ($li.siblings().length > 0) {
-		$remove.removeAttr('disabled');
+	console.log(specificity);
+
+	if (specificity) {
+		console.log('Tool.calculate: ');
+
+		var html = '<span class="specificity-a">' + specificity.values.a + '</span>,' +
+				   '<span class="specificity-b">' + specificity.values.b + '</span>,' +
+				   '<span class="specificity-c">' + specificity.values.c + '</span>,' +
+				   '<span class="specificity-d">' + specificity.values.d + '</span>';
+
+		$span.html(html);
+		$li.data('specificity', specificity.string);
+		$li.data('specificity-matches', specificity.matches);
+
+		console.log("data spec: " + $li.data('specificity'));
+
+		$clear.removeAttr('disabled');
+		if ($li.siblings().length > 0) {
+			$remove.removeAttr('disabled');
+		}
 	}
 
 }
@@ -30,31 +71,43 @@ Tool.calculate = function () {
 
 Tool.newRow = function () {
 
-	var $list = $('.selectors'),
-		$newRow = $list.find('li:last').clone();
+	var $newRow = Tool.$list.find('li:last').clone();
 
-	$newRow.find('.specificity-string').html($newRow.find('.specificity-string').data('default'));
+	$newRow.find('.specificity-string span').each(function () {
+		$(this).data('default');
+	});
 	$newRow.find('input[name="selector"]').val("");
 	$newRow.find('.clear').attr('disabled', 'disabled');
 
 	$newRow.find('.remove').removeAttr('disabled');
-	$list.find('.remove').removeAttr('disabled');
-	$list.append($newRow);
+	Tool.$list.find('.remove').removeAttr('disabled');
+	Tool.$list.append($newRow);
+
+	if (Tool.$list.children().length > 1) {
+		Tool.$sort.removeAttr('disabled');
+	} else {	
+		Tool.$sort.attr('disabled', 'disabled');	
+	}
 
 }
 
 
 Tool.removeRow = function () {
 
-	var $li = $(this).parent(),
-		$list = $li.parent();
+	var $li   = $(this).parent();
 
 	$li.remove();
 
-	if ($li.siblings().length > 0) {
-		$list.find('.remove').removeAttr('disabled');
+	if (Tool.$list.children().length > 1) {
+		Tool.$list.find('.remove').removeAttr('disabled');
 	} else {
-		$list.find('.remove').attr('disabled', 'disabled');
+		Tool.$list.find('.remove').attr('disabled', 'disabled');
+	}
+
+	if (Tool.$list.children().length > 1) {
+		Tool.$sort.removeAttr('disabled');
+	} else {	
+		Tool.$sort.attr('disabled', 'disabled');	
 	}
 
 }
@@ -65,6 +118,81 @@ Tool.clearRow = function () {
 	var $li = $(this).parent();
 
 	$li.find('input[name="selector"]').val("");
-	$li.find('.specificity-string').html($li.find('.specificity-string').data('default'));
+	$li.find('.specificity-string')
+		.html(Tool.onResetDefaultTo);
+
+}
+
+
+Tool.sortRows = function () {
+
+	
+	Tool.$list.children().each(function () {
+		Tool.calculate($(this));
+	})
+	
+	var newOrder = Tool.$list.children().sort(Tool.sort);
+
+	Tool.$list.children().remove();
+
+	for (var i = 0; i < newOrder.length; i++) {
+		Tool.$list.append(newOrder[i]);
+	}
+
+}
+
+
+Tool.sort = function (liA, liB) {
+
+	var specA = $(liA).data('specificity').split(','),
+	    specB = $(liB).data('specificity').split(',');
+
+	for (var i = 0; i < specA.length; i++) {
+
+		if (specA[i] < specB[i]) {
+			return 1;
+		} else if (specA[i] > specB[i]) {
+			return -1;
+		}
+	}
+
+	return 0;
+
+}
+
+
+Tool.showMatchesTooltip = function () {
+
+	console.log($(this).parent().parent().data('specificity-matches'));
+
+	var $this      = $(this),
+		part       = "", 
+		partResult = [],
+		html       = "",
+		matches    = $this.parent().parent().data('specificity-matches');
+
+	partResult = $this.attr('class').match(/^specificity-(a|b|c|d).*/i);
+	part = partResult[1];
+
+	if (matches) {
+		if (matches[part].length > 0) {
+			html = '<ul><li>' + matches[part].join('</li><li>') + '</li></ul>';
+			console.log(matches[part]);
+			$this.append(html).find('ul').fadeIn('fast');
+		}
+	} else {
+		// not yet calculated
+	}
+
+}
+
+
+Tool.hideMatchesTooltip = function () {
+
+	var $ul = $(this).parentsUntil('li').parent().find('.specificity-string ul');
+
+	$ul.fadeOut('fast', function () {
+		remove();
+	});
 
 }
